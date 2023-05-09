c9070298c57e5a55988fe091f179b8ad841f8350
Okai Larbi Ernest <elokai6175@gmail.com>
2023-05-09 20:03:21 +0000

    3 pages done

--- public/index.js
+++ public/index.js
@@ -3,4 +3,15 @@ const nav = document.querySelector("nav ul");
 
 menu.addEventListener("click", () => {
   nav.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 1024) {
+    nav.classList.remove("hidden");
+    menu.classList.add("hidden");
+  } else {
+    nav.classList.add("hidden");
+    menu.classList.remove("hidden");
+  }
 });
